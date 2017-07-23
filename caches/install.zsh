9c2b9bd18de9da8bc97d5e20685f0e619cd08819
Alexandre Proença <alexandre.proenca@hotmail.com.br>
2017-07-22 22:43:20 -0300

    Update install.zsh

--- caches/install.zsh
+++ caches/install.zsh
@@ -1,6 +1,6 @@
 # cached-pip
-export PIP_TRUSTED_HOST=devpi.axado.com.br
-export PIP_INDEX_URL=http://hackerberg:chucknorris@${PIP_TRUSTED_HOST}/hackerberg/dev/+simple/
+#export PIP_TRUSTED_HOST=devpi.axado.com.br
+#export PIP_INDEX_URL=http://hackerberg:chucknorris@${PIP_TRUSTED_HOST}/hackerberg/dev/+simple/
 
 # cached-npm
 # export NPM_CONFIG_PROXY=http://hack-01:8080/
